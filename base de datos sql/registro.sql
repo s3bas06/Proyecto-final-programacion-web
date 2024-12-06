9b42fd6f0ee04a3602fe2cef789eea6cb575df9c
s3bas06 <sebastianoa06@gmail.com>
2024-12-05 18:09:12 -0700

    Update registro.sql

--- base de datos sql/registro.sql
+++ base de datos sql/registro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2024 a las 21:34:44
+-- Tiempo de generación: 06-12-2024 a las 02:08:34
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
